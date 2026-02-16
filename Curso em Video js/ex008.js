299c7ea27d304e2d1cb1444d7792c8369a1c9e12
Yago Badu  Campolongo <yago.campolongo@gmail.com>
2026-02-16 16:10:35 -0300

    ex09

--- Curso em Video js/ex008.js
+++ Curso em Video js/ex008.js
@@ -1,6 +1,12 @@
-console.log ('O console funcionou corretamente')
 
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+    if (vel > 60) { // condição simples!
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`Dirija sempre com cinto de segurança!`);
 
 // var vel = 78.2
 // console.log(`A velocidade do seu carro é ${vel}km/h`)
 // if (vel > 60) {  }
+   
---
+++ ex009.js
@@ -0,0 +1,23 @@
+var pais = 'italia'
+console.log(`Vivendo em ${pais}`)
+    if (pais != 'Brasil') {
+        console.log(`Voce é estrangeiro`)
+    } else {
+        console.log(`Voce é brasileiro!`)
+    }
+
+
+
+
+
+
+/*var pais = 'italia'
+console.log(`Vivendo em ${pais}`)
+    if (pais == 'Brasil') {
+        console.log(`Voce é brasileiro`)
+    } else {
+        console.log(`Voce é estrangeiro!`)
+    } */
+
+
+   
